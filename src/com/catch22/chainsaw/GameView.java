9a6c2a179520e5ae12d1956faec8564e306d85a4
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-09-18 21:30:58 +0200

    Made the GameView use some native code.

--- src/com/catch22/chainsaw/GameView.java
+++ src/com/catch22/chainsaw/GameView.java
@@ -2,6 +2,7 @@ package com.catch22.chainsaw;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class GameView extends Activity {
@@ -10,11 +11,18 @@ public class GameView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
+        GameView.createGLRenderer();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_game_view, menu);
         return true;
+    }
+    
+    public static native void createGLRenderer();
+    
+    static {
+    	System.loadLibrary("catchLib");
     }
 }
